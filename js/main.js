eea219d14d55e478f6ccc58174c8c8a382aef960
Gastón <avila.gas@gmail>
2013-08-07 00:01:34 -0300

    added requirejs to the mix

---
+++ js/main.js
@@ -0,0 +1,35 @@
+require.config({
+    baseUrl: '../js',
+    shim: {
+        OpenLayers: {
+            exports: 'OpenLayers'
+        },
+        handlebars: {
+            exports: 'Handlebars'
+        }
+    },
+    paths: {
+        "backbone": "lib/backbone-amd/backbone",
+        "jquery": "lib/jquery/jquery",
+        "OpenLayers": "lib/OpenLayers",
+        // "OpenLayers": "lib/openlayers/lib/OpenLayers",
+        "underscore": "lib/underscore-amd/underscore",
+        "handlebars": "lib/handlebars/handlebars"
+    }
+});
+
+require(["transit/model",
+        "transit/ui", 
+        "transit/config"], function (model, ui, config) {
+    'use strict';
+
+    var dataModel;
+
+    dataModel = model.init();
+
+    ui.init({
+        controls: 'editor'
+    });
+
+
+});
