c1773e65c8ee9a56f53b5a9f12afa2cfb261cb8b
Gastón <avila.gas@gmail>
2013-08-12 00:55:18 -0300

    translate to geojson, rewrite map view and tags

---
+++ js/models/tags.js
@@ -0,0 +1,16 @@
+define([
+	"underscore",
+	"backbone"],
+function (_, Backbone) {
+
+	console.log("tags model");
+
+	var TagsModel = Backbone.Model.extend({
+
+		initialize: function () {
+		}
+
+	}); 
+
+	return TagsModel;
+});
