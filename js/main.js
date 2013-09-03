87a7c287e0f7081b9c3720cf04a636e686dfde7f
Gastón <avila.gas@gmail>
2013-09-03 07:44:35 -0300

    fix bower dependencies

--- js/main.js
+++ js/main.js
@@ -9,12 +9,12 @@ require.config({
         }
     },
     paths: {
-        "backbone": "../../bower_components/backbone-amd/backbone",
-        "jquery": "../../bower_components/jquery/jquery",
+        "backbone": "../bower_components/backbone-amd/backbone",
+        "jquery": "../bower_components/jquery/jquery",
         "OpenLayers": "lib/OpenLayers/OpenLayers",
-        "underscore": "../../bower_components/underscore-amd/underscore",
-        "handlebars": "../../bower_components/handlebars/handlebars",
-        "text": "../../bower_components/requirejs-text/text"
+        "underscore": "../bower_components/underscore-amd/underscore",
+        "handlebars": "../bower_components/handlebars/handlebars",
+        "text": "../bower_components/requirejs-text/text"
     }
 });
 
