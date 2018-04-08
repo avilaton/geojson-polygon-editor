76bb0fb663faeefdfac4628548291d20f2b56b91
Gaston Avila <avila.gas@gmail.com>
2018-04-07 22:23:52 -0300

    agregar angularjs

--- geojson_editor/static/js/editor.js
+++ geojson_editor/static/js/editor.js
@@ -1,5 +1,22 @@
 var OpenLayers = require('script-loader!./lib/OpenLayers/OpenLayers.js')
 var LayoutView = require('./views/editor-layout')
+var angular = require('angular')
+
+var app = angular.module('app', [])
+
+app.component("app", {
+    template: `
+<ul>
+    <li ng-repeat="prod in $ctrl.products">{{prod}}</li>
+</ul>
+`,
+    controller: [function() {
+        this.products = ["Milk", "Bread", "Cheese"];
+        
+    }]
+});
+
+angular.bootstrap(document.getElementById('app'), ['app'], { strictDi: true })
 
 'use strict';
 
